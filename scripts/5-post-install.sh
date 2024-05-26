55ca610c79576a99619c3b1c65bd68a14e52c1f7
anisbsalah <anisbensalah.89@gmail.com>
2024-05-26 01:58:36 +0000

    initramfs at the end of script

--- scripts/5-post-install.sh
+++ scripts/5-post-install.sh
@@ -87,6 +87,21 @@ sed -i 's/^[#[:space:]]*\(%wheel[[:space:]]*ALL=(ALL:ALL)[[:space:]]*ALL\)$/\1/g
 
 echo "
 ==============================================================================
+ Initramfs
+==============================================================================
+"
+# Initramfs
+if [[ ${FILESYSTEM} == btrfs ]]; then
+	sed -i 's/^MODULES=()/MODULES=(btrfs)/' /etc/mkinitcpio.conf
+fi
+sed -i 's/^BINARIES=()/BINARIES=(setfont)/' /etc/mkinitcpio.conf
+# sed -i 's/^\(HOOKS=["(]*base .*\) keymap consolefont \(.*\)$/\1 sd-vconsole \2/g' /etc/mkinitcpio.conf
+sed -i '/^HOOKS=/s/autodetect\( \|$\)/autodetect microcode\1/g' /etc/mkinitcpio.conf
+sed -i 's/^[#[:space:]]*COMPRESSION="zstd"/COMPRESSION="zstd"/' /etc/mkinitcpio.conf
+mkinitcpio -P
+
+echo "
+==============================================================================
  Cleaning
 ==============================================================================
 "
